bd3cf138209df8c4085610237b99431f30afbd7b
diogommartins <magalhaesmartins@icloud.com>
2019-02-01 12:52:58 -0200

    #44 - Removing unused code

--- aiologger/handlers/files.py
+++ aiologger/handlers/files.py
@@ -152,54 +152,6 @@ class BaseAsyncRotatingFileHandler(AsyncFileHandler, metaclass=abc.ABCMeta):
             self.rotator(source, dest)
 
 
-class AsyncRotatingFileHandler(BaseAsyncRotatingFileHandler):
-    """
-    Handler for logging to a set of files, which switches from one file
-    to the next when the current file reaches a certain size.
-    """
-
-    def __init__(
-        self,
-        filename: str,
-        mode: str = "a",
-        max_bytes: int = 0,
-        backup_count: int = 0,
-        encoding: str = None,
-        namer: Namer = None,
-        rotator: Rotator = None,
-    ) -> None:
-        """
-        Open the specified file and use it as the stream for logging.
-
-        By default, the file grows indefinitely. You can specify particular
-        values of `max_bytes` and `backup_count` to allow the file to rollover
-        at a predetermined size.
-
-        Rollover occurs whenever the current log file is nearly `max_bytes` in
-        length. If `backup_count` is >= 1, the system will successively create
-        new files with the same pathname as the base file, but with extensions
-        ".1", ".2" etc. appended to it. For example, with a `backup_count` of 5
-        and a base file name of "app.log", you would get "app.log",
-        "app.log.1", "app.log.2", ... through to "app.log.5". The file being
-        written to is always "app.log" - when it gets filled up, it is closed
-        and renamed to "app.log.1", and if files "app.log.1", "app.log.2" etc.
-        exist, then they are renamed to "app.log.2", "app.log.3" etc.
-        respectively.
-
-        If `max_bytes` is zero, rollover never occurs.
-        """
-        if max_bytes > 0 and mode != "a":
-            # If rotation/rollover is wanted, it doesn't make sense to use
-            # another mode. If for example 'w' were specified, then if there
-            # were multiple runs of the calling application, the logs from
-            # previous runs would be lost if the 'w' is respected, because the
-            # log file would be truncated on each run.
-            raise ValueError("max_bytes can't be set > 0 if mode isn't \"a\"")
-        super().__init__(filename, mode, encoding, namer, rotator)
-        self.max_bytes = max_bytes
-        self.backup_count = backup_count
-
-
 class RolloverInterval(str, enum.Enum):
     SECONDS = "S"
     MINUTES = "M"
