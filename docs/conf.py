15c76bcce8fb2ff54cf9444e3f34b104c0138ebd
diogommartins <magalhaesmartins@icloud.com>
2019-02-01 12:23:44 -0200

    #44 - Adding markdown support for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@
 
 
 # -- Project information -----------------------------------------------------
+from recommonmark.parser import CommonMarkParser
 
 project = "aiologger"
 copyright = "2019, aiologger contributors"
@@ -51,8 +52,9 @@ templates_path = ["_templates"]
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = ".rst"
+source_suffix = [".rst", ".md"]
+
+source_parsers = {".md": CommonMarkParser}
 
 # The master toctree document.
 master_doc = "index"
