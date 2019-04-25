e133381bc74e9591e0b60be84141347e3a91b3a8
Diogo Magalhães Martins <diogommartins.dev@gmail.com>
2019-04-25 19:39:38 -0300

    #56 Adding a test utility function for creating LogRecord instances

---
+++ tests/utils.py
@@ -0,0 +1,19 @@
+from aiologger.levels import LogLevel
+from aiologger.records import LogRecord
+
+
+def make_log_record(**kwargs) -> LogRecord:
+    """
+    Make a LogRecord whose attributes are defined by the specified kwargs
+    """
+    record = LogRecord(
+        name=None,
+        level=LogLevel.NOTSET,
+        pathname="",
+        lineno=0,
+        msg="",
+        args=(),
+        exc_info=None,
+    )
+    record.__dict__.update(kwargs)
+    return record
